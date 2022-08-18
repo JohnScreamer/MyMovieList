2bc824b8e5ed63f97e8ec5860f5a21191df3c8e1
JohnScreamer <bogaturscreamer@gmail.com>
2022-08-18 20:15:01 +0300

    fix

--- src/components/MovieComponents/TopRatated/TopratedMovie.tsx
+++ src/components/MovieComponents/TopRatated/TopratedMovie.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useMovieDiscoverQuery } from "../../../redux/RTQK/KinoList";
 import {
     selectApiLanguage,
@@ -6,6 +6,7 @@ import {
     selectorLanguage,
 } from "../../../selectors/GlobalOptions";
 import { useAppSelector } from "../../../static/hooks/hooks";
+import { DiscoveryMovieResponse } from "../../../Types/DiscoveryType";
 import DiscoverCard from "../../cards/DiscoverCard/DiscoverCard";
 import ErrorPopUp from "../../UI/ErrorPopUp/ErrorPopUp";
 import Spiner from "../../UI/Spiner/Spiner";
@@ -13,25 +14,31 @@ import s from "./TopratedMovie.module.scss";
 const TopratedMovie = () => {
     const apiParam = useAppSelector(selectorApiOptions);
     const language = useAppSelector(selectApiLanguage);
-    const { data, isError, isLoading } = useMovieDiscoverQuery({
-        param: {
-            ...apiParam,
-            language: language,
-            sort_by: "vote_average.desc",
-            "vote_count.gte": 1000,
-        },
-        url: "",
-    });
+    const [data, setData] = useState<DiscoveryMovieResponse | null>(null);
+    // const { data, isError, isLoading } = useMovieDiscoverQuery({
+    //     param: {
+    //         ...apiParam,
+    //         language: language,
+    //         sort_by: "vote_average.desc",
+    //         "vote_count.gte": 1000,
+    //     },
+    //     url: "",
+    // });
+    useEffect(() => {
+        fetch(
+            "https://api.themoviedb.org/3/discover/movie?language=en-US&api_key=48bce37aca39640849c2b1ca0861e9b9&sort_by=vote_average.desc&vote_count.gte=1000"
+        )
+            .then((data) => data.json())
+            .then((data) => setData(data));
+    }, []);
     const topRatedList = data?.results.map((el, id) => (
         <DiscoverCard movie={el} id={id + 1} key={id} />
     ));
 
     return (
         <div className={s.wrapper}>
-            {isLoading ? <Spiner /> : topRatedList}
-            {isError && (
-                <ErrorPopUp isError text={"Error, cant get top rated movie."} />
-            )}
+            {/* <ErrorPopUp isError text={"Error, cant get top rated movie."} /> */}
+            {topRatedList}
         </div>
     );
 };
