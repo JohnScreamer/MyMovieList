846457e06fe5754842afdaffa8f6d3d5ddede6b1
JohnScreamer <bogaturscreamer@gmail.com>
2022-08-21 22:09:51 +0300

    fix

--- src/components/UI/Img/ImgWrapper.tsx
+++ src/components/UI/Img/ImgWrapper.tsx
@@ -1,19 +1,22 @@
 import { Skeleton } from "@mui/material";
 import { useState, useEffect, FC } from "react";
+import { selectorApiOptions } from "../../../selectors/GlobalOptions";
+import { useAppSelector } from "../../../static/hooks/hooks";
 import Spiner from "../Spiner/Spiner";
 import ZoomImg from "../ZoomImg/ZoomImg";
 type ImgType = {
     imgUrl: string;
 };
 const Img: FC<ImgType> = ({ imgUrl }) => {
+    const param = useAppSelector(selectorApiOptions);
     const [url, setUrl] = useState("");
     useEffect(() => {
-        fetch(imgUrl)
+        fetch(imgUrl, { mode: "no-cors" })
             .then((response) => response.blob())
             .then((image) => {
                 setUrl(URL.createObjectURL(image));
             });
-    }, []);
+    }, [imgUrl]);
 
     if (!url) {
         return (
