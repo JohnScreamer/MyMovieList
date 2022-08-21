b2de3d1f9e1bcb7634716147041519bce332a6df
JohnScreamer <bogaturscreamer@gmail.com>
2022-08-21 23:53:28 +0300

    fix

--- src/components/UI/Img/ImgWrapper.tsx
+++ src/components/UI/Img/ImgWrapper.tsx
@@ -1,4 +1,5 @@
 import { Skeleton } from "@mui/material";
+import axios from "axios";
 import { useState, useEffect, FC } from "react";
 import { selectorApiOptions } from "../../../selectors/GlobalOptions";
 import { useAppSelector } from "../../../static/hooks/hooks";
@@ -10,36 +11,36 @@ type ImgType = {
 const Img: FC<ImgType> = ({ imgUrl }) => {
     const param = useAppSelector(selectorApiOptions);
     const [url, setUrl] = useState("");
-    // useEffect(() => {
-    //     fetch(imgUrl, {
-    //         mode: "cors",
-    //         headers: {
-    //             "Access-Control-Allow-Origin": "http://localhost:3000",
-    //             "Access-Control-Allow-Credentials": "true",
-    //         },
-    //     })
-    //         .then((response) => {
-    //             console.log(response);
+    useEffect(() => {
+        // fetch(imgUrl)
+        //     .then((response) => {
+        //         console.log(response);
+        //         return response.blob();
+        //     })
+        //     .then((image) => {
+        //         setUrl(URL.createObjectURL(image));
+        //     });
 
-    //             return response.blob();
-    //         })
-    //         .then((image) => {
-    //             setUrl(URL.createObjectURL(image));
-    //         });
-    // }, [imgUrl]);
+        fetch(
+            "https://image.tmdb.org/t/p/w500/47X1h91UUfhMxO42KV9fKoGAVMC.jpg",
+            { method: "GET" }
+        )
+            .then((data) => data.blob())
+            .then((data) => setUrl(URL.createObjectURL(data)));
+    }, [imgUrl]);
 
-    // if (!url) {
-    //     return (
-    //         <div>
-    //             <Spiner small />
-    //         </div>
-    //     );
-    // }
+    if (!url) {
+        return (
+            <div>
+                <Spiner small />
+            </div>
+        );
+    }
 
     return (
         <>
-            <img src={imgUrl} />
-            <ZoomImg url={imgUrl} />
+            <img src={url} />
+            <ZoomImg url={url} />
         </>
     );
 };
